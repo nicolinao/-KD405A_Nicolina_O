518b278db649a5f1b59ffaa7eb349f8c613e7e41
nicolinao <anilocin@hotmail.com>
2016-03-09 13:11:44 +0100

    Assignment7

---
+++ assign7/src/A7_Lars_H/src/TryIt.java
@@ -0,0 +1,57 @@
+package A7_Lars_H.src;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import se.mah.k3.klara.PixelController;
+import se.mah.k3.klara.PixelController.Screen;
+
+public class TryIt extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TryIt frame = new TryIt();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TryIt() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		PixelController px = PixelController.getInstance(Screen.MEDEA_1);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		px.setPixel(10, 10, Color.RED);
+		px.setPixel(10, 11, Color.WHITE);
+		px.setPixel(10, 12, Color.BLUE);
+		px.setPixel(10, 13, Color.GREEN);
+	}
+
+}
